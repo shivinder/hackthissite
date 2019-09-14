3030ae6364495901cdc1f7f9932f50410b3e6a21
shavitoor <shivinder@gmail.com>
2019-09-14 20:57:18 +1000

    working version of the script.py

--- Programming/2/script.py
+++ Programming/2/script.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
 
-with open('PNG.png','rb') as picture_binary:
-    for line in picture_binary:
-        print(line)
+from PIL import Image
+import translator
+
+# filename here
+# https://www.hackthissite.org/missions/prog/2/PNG.png
+png_filename = 'PNG.png'
+
+# initialise the morse decoder here
+decoder = translator.Decoder()
+
+# start working with the image
+png_image_bytes = Image.open(png_filename,'r').tobytes()
+
+# declare the counter
+counter, offset = 0, 0
+morse_code = ''
+
+# begin traversal through the bytes
+for byte in png_image_bytes:
+    
+    # this is the ascii code
+    if byte == 1:
+        morse_code += chr((counter-offset))
+        offset = counter
+
+    counter += 1
+
+# print the morse code
+print('Morse Code: {}'.format(morse_code))
+
+# decode the morse code
+print('Plain Text: {}'.format(decoder.decode(morse_code).plaintext))
